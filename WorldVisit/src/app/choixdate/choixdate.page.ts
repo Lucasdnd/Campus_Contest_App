e6bec4dc7d7ccc957ee270bf5316a1274205d8f2
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2021-02-19 18:11:32 +0100

    modification calendrier

--- WorldVisit/src/app/choixdate/choixdate.page.ts
+++ WorldVisit/src/app/choixdate/choixdate.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-choixdate',
   templateUrl: './choixdate.page.html',
   styleUrls: ['./choixdate.page.scss'],
 })
-export class ChoixdatePage implements OnInit {
+export class ChoixdatePage {
+  searchName:'';
+  countryApiUrl = '';
+  countryData = {
+    name: '',
+    capital:'',
+    region:''
+  };
+  countries: Object;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public http: HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+    readAPI(URL: string) {
+      return this.http.get(URL);
+    }
+
+    searchCountry() {
+      const search = encodeURIComponent(this.searchName);
+      console.log('recherche du pays '+ this.searchName);
+      this.countryApiUrl = 'https://restcountries.eu/rest/v2/name/' + search;
+      this.readAPI( this.countryApiUrl)
+    .subscribe((data) => {
+      console.log(data[0]['name']);
+      console.log(data[0]['capital']);
+      console.log(data[0]['region']);
+      this.countries = data;
+      this.countryData.name = data[0]['name'];
+      this.countryData.capital = data[0]['capital'];
+      this.countryData.region = data[0]['region'];
+    });
+    }
 }
