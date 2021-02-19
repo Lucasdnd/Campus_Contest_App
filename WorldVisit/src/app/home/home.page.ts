15b48ea4c6971beb5a10e960561cd434abfecfe9
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2021-02-19 17:10:59 +0100

    search

--- WorldVisit/src/app/home/home.page.ts
+++ WorldVisit/src/app/home/home.page.ts
@@ -8,20 +8,34 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  searchName:'';
   countryApiUrl = '';
   countryData = {
-    
-  }
+    name: '',
+    capital:'',
+    region:''
+  };
 
   constructor(public http: HttpClient) {
-    this.readAPI('https://restcountries.eu/rest/v2/all')
-    .subscribe((data) => {
-      console.log(data);
-    });
+    
   }
 
     readAPI(URL: string) {
       return this.http.get(URL);
     }
 
+    searchCountry() {
+      const search = encodeURIComponent(this.searchName);
+      console.log('recherche du pays '+ this.searchName);
+      this.countryApiUrl = 'https://restcountries.eu/rest/v2/name/' + search;
+      this.readAPI( this.countryApiUrl)
+    .subscribe((data) => {
+      console.log(data[0]['name']);
+      console.log(data[0]['capital']);
+      console.log(data[0]['region']);
+      this.countryData.name = data[0]['name'];
+      this.countryData.capital = data[0]['capital'];
+      this.countryData.region = data[0]['region'];
+    });
+    }
 }
